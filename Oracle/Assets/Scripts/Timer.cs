23178d79069a99a24f3444ac7b9e45beaf5c39c5
Marissa <mlwalthe@mtu.edu>
2018-10-07 19:54:57 -0400

    Added Time Spent on Level prefab and scripts

---
+++ Oracle/Assets/Scripts/Timer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
+public class Timer : MonoBehaviour {
+	public Text TimerText;
+	float timer = 0;
+	// Use this for initialization
+	void Start () {
+		timer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		TimerText.text = ("Time Spent on Level: " + (int)timer);
+	}
+
+	
+}
