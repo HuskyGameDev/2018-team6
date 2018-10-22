50bdb9bd34d6cdbc4a4d505e3f438b716c53cfd6
Marissa <mlwalthe@mtu.edu>
2018-10-21 20:34:30 -0400

    Score Added
    Score added based on time spent on level

--- Oracle/Assets/Scripts/Timer.cs
+++ Oracle/Assets/Scripts/Timer.cs
@@ -5,7 +5,10 @@ using UnityEngine.UI;
 using System.Threading;
 public class Timer : MonoBehaviour {
 	public Text TimerText;
+	public Text ScoreText;
 	float timer = 0;
+	int i = 0;
+	float score = 0;
 	// Use this for initialization
 	void Start () {
 		//timer will alwwawys start at 0
@@ -15,10 +18,29 @@ public class Timer : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		//update time by seconds
+		if ( i == 0) {
+		if ( timer > 3 ) {
+			Score();
+			 Time.timeScale = 0;
+		}
+	
 		timer += Time.deltaTime;
 		//display
 		TimerText.text = ("Time Spent on Level: " + (int)timer + " Seconds");
+		}
 	}
+
+	//score based on time
+	void Score() {
+		if ( i == 0) {
+		 score = ((5 * 60) - timer) * 10;
+		 ScoreText.text = ("Total score: " + (int) score + " Points");
+		 Time.timeScale = 0;
+		 i++;
+		}
+
+	}
+
 
 
 }
