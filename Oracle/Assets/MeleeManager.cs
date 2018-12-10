956c65f5a5f29045957c161655122bf89b55a5d6
Marissa <mlwalthe@mtu.edu>
2018-12-10 14:31:38 -0500

    Revert "Revert "Merge branch 'master' of https://github.com/HuskyGameDev/2018-team6""
    This reverts commit a4d0e2fc3b92ab3ae28f2b608e54b666c02ea0cd.

--- Oracle/Assets/MeleeManager.cs
+++ Oracle/Assets/MeleeManager.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class MeleeManager : MonoBehaviour {
 
+    public float range;
+    public LayerMask myLayerMask;
+    
+
+
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +17,24 @@ public class MeleeManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Attack();
+        }
 		
 	}
+
+    // method that attacks and broadcasts the hit
+    void Attack()
+    {
+        RaycastHit hit;
+       
+        if(Physics.Raycast(transform.position, transform.forward, out hit, range, myLayerMask))
+        {
+          //  Target target = hit.transform.gameObject.GetComponent<Target>)();
+            Debug.Log(hit.transform.name);
+            
+        }
+        
+    }
 }
